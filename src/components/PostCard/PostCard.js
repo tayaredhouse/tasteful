7c7d07bd33ae33528b43b0f6eea9c008c0ca298e
Redhouse <85511085+tayaredhouse@users.noreply.github.com>
2021-11-29 15:11:38 -0800

    render images, title, and author info on PostCard

--- src/components/PostCard/PostCard.js
+++ src/components/PostCard/PostCard.js
@@ -1,11 +1,20 @@
 import React from 'react'
 
-function PostCard() {
+ function PostCard(props) {
+    const {data} = props;
+
+
     return (
-        <div>
-            PostCard
+        <div className ="card-item">
+            <div className="card-inner">
+                <div className="card-top">
+                <h3>{data.title} </h3>
+                <p>By {data.author}</p>
+                <img src = {data.url}/>
+                </div>
+            </div>
         </div>
     )
-}
+    }
 
 export default PostCard
